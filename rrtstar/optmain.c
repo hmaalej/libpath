90998ce481f4b6d36e55f897d88f6f1c4ba27292
hmaalej <houssemaalej@gmail.com>
2012-08-20 16:43:32 +0200

    last_all_in main

--- rrtstar/optmain.c
+++ rrtstar/optmain.c
@@ -43,6 +43,32 @@ log_and_exit1(const char *fmt, ...) {
 	exit(1);
 }
 
+/*la je vide le main pour le decomposer en plusieurs fonctions:
+	1)une fonction chemin_sans_corespondance(state* root_state, region_2d_t goal_region)
+		renvoie un .txt contenant les points du chemin
+	2)une fonction route final(state* root_state, region_2d_t goal_region);
+		prend les 2 points de departs et d'arriver en parametre et verifie s'il faut faire une correspondance:
+			-en general on decoupe le chemin en 2 si un point est dans les regions scandinaves et l'autre ne l'est pas
+			-on verra d'autres possibilité
+			
+		elle appele chemin_sans_correspondance autant de fois que necessaire
+		
+		renvoie un .txt et .geojson qui contiennent le resultat final   
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main (int argc, char *argv[]) {
     
     int64_t t = ts_now();
@@ -64,6 +90,12 @@ int main (int argc, char *argv[]) {
     
     // 2. Setup the environment
     // 2.a. create the operating region
+    double dist_x = atof(argv[1])-atof(argv[3]);
+    double dist_y = atof(argv[2])-atof(argv[4]);
+
+    double dist = sqrt (dist_x * dist_x + dist_y * dist_y);
+    if (dist<20)
+	k=1;
  int c1,c2,s1,s2;
     if ((atof(argv[1])>-10) && (atof(argv[1])<50) && (atof(argv[2])>30) && (atof(argv[2])<50) && (atof(argv[3])>-10) && (atof(argv[3])<50) && (atof(argv[4])>30) && (atof(argv[4])<50)) {
 	c1= 20/k;
@@ -74,7 +106,7 @@ int main (int argc, char *argv[]) {
     else  if ((atof(argv[1])>16) && (atof(argv[1])<30) && (atof(argv[2])>54) && (atof(argv[2])<66) && (atof(argv[3])>16) && (atof(argv[3])<30) && (atof(argv[4])>54) && (atof(argv[4])<66)) {
 	
 	c1= 23/k;
-	c2= 3;
+	c2= 60/k;
 	s1= 14/k;
 	s2= 12/k;
     }	
@@ -109,32 +141,7 @@ int main (int argc, char *argv[]) {
 	int k_;
 	int cpt_continent=1;
 
-/*while(cpt_continent<13){	
-    while( fscanf(f_obstacles,"[ %lf, %lf ]",&x,&y)!=0){
-    	node = malloc (sizeof (state_t));
-  	node->x[0] = x/20;
-    	node->x[1] = y/20;
-       	obstacle=g_slist_prepend(obstacle,node);
-    }
-	cs= GEOSCoordSeq_create(g_slist_length (obstacle),2);
-	int j=0;
-	while(obstacle) {
-		state_t *point=(state_t *)(obstacle->data);
-		k=GEOSCoordSeq_setX(cs, j, point->x[0]);
-		k=GEOSCoordSeq_setY(cs, j, point->x[1]);
-		obstacle=g_slist_next(obstacle);
-		j++;
-	}
-	
-	
-	shell = GEOSGeom_createLinearRing(cs);
-	g = GEOSGeom_createPolygon(shell, NULL, 0);
-	    
-	 
-	obstacle_list = g_slist_prepend (obstacle_list, g);
-    fscanf(f_obstacles,"\n\n");
-    cpt_continent++;
-}*/
+
 
 fgets(chaine, 2, f_obstacles);
 fscanf(f_obstacles,"\n");
@@ -277,75 +284,7 @@ cpt_poly++;
     	};
     opttree_set_root_state (opttree, &root_state);
     optsystem_update_goal_region (opttree->optsys, &goal_region);
- /*
-    //2.e rectangle
-GSList *rectangle_list = NULL;
-GEOSCoordSequence* cs3;
-	GEOSGeometry* g3;  
-cs3= GEOSCoordSeq_create(5,2);
 
-k=GEOSCoordSeq_setX(cs3, 0, 2.1);
-k=GEOSCoordSeq_setY(cs3, 0, 1.535);
-
-k=GEOSCoordSeq_setX(cs3, 1, 5.8);
-k=GEOSCoordSeq_setY(cs3, 1, 1.535);
-
-k=GEOSCoordSeq_setX(cs3, 2, 5.8);
-k=GEOSCoordSeq_setY(cs3, 2, 3.29);
-
-k=GEOSCoordSeq_setX(cs3, 3, 2.1);
-k=GEOSCoordSeq_setY(cs3, 3, 3.29);
-
-k=GEOSCoordSeq_setX(cs3, 4, 2.1);
-k=GEOSCoordSeq_setY(cs3, 4, 1.535);
-
-
-shell = GEOSGeom_createLinearRing(cs3);
-g3 = GEOSGeom_createPolygon(shell, NULL, 0);	   
-    rectangle_list=g_slist_prepend(rectangle_list,g3);
-
-k=GEOSCoordSeq_setX(cs3, 0, -0.5);
-k=GEOSCoordSeq_setY(cs3, 0, 0.315);
-
-k=GEOSCoordSeq_setX(cs3, 1, 1.66);
-k=GEOSCoordSeq_setY(cs3, 1, 0.315);
-
-k=GEOSCoordSeq_setX(cs3, 2, 1.66);
-k=GEOSCoordSeq_setY(cs3, 2, 1.5);
-
-k=GEOSCoordSeq_setX(cs3, 3, -0.5);
-k=GEOSCoordSeq_setY(cs3, 3, 1.5);
-
-k=GEOSCoordSeq_setX(cs3, 4, -0.5);
-k=GEOSCoordSeq_setY(cs3, 4, 0.315);
-
-
-shell = GEOSGeom_createLinearRing(cs3);
-g3 = GEOSGeom_createPolygon(shell, NULL, 0);	   
-    rectangle_list=g_slist_prepend(rectangle_list,g3);
-
-k=GEOSCoordSeq_setX(cs3, 0, 0.69);
-k=GEOSCoordSeq_setY(cs3, 0, -0.95);
-
-k=GEOSCoordSeq_setX(cs3, 1, 1.8);
-k=GEOSCoordSeq_setY(cs3, 1, -0.95);
-
-k=GEOSCoordSeq_setX(cs3, 2, 1.8);
-k=GEOSCoordSeq_setY(cs3, 2, 0.315);
-
-k=GEOSCoordSeq_setX(cs3, 3, 0.69);
-k=GEOSCoordSeq_setY(cs3, 3, 0.315);
-
-k=GEOSCoordSeq_setX(cs3, 4, 0.69);
-k=GEOSCoordSeq_setY(cs3, 4, -0.95);
-
-
-shell = GEOSGeom_createLinearRing(cs3);
-g3 = GEOSGeom_createPolygon(shell, NULL, 0);	   
-    rectangle_list=g_slist_prepend(rectangle_list,g3);
-
- optsystem_update_rectangles (opttree->optsys, rectangle_list);
-*/
 finishGEOS();
 	
     // 3. Run opttree in iterations
@@ -374,8 +313,7 @@ else {
 	    if ((opttree->lower_bound<99999) && (b==FALSE)){
 		b=TRUE;
 		
-		/*x=i/1000;
-		num_iterations=i+(i*40*log(x))/(x*x);*/
+		
 		if ((opttree->lower_bound)*k<50)
 		num_iterations=i+502;
 		if (((opttree->lower_bound)*k>=50) && ((opttree->lower_bound)*k<200))
@@ -389,10 +327,7 @@ else {
 		if (i>50000)
 		num_iterations=i+502;
 		ts_find=(ts_now() - time_start)/1000000.0;
-			//il y'a trés peu d'iteration
-		//if (i<3000)
-		//num_iterations=i+1002;
-		//num_iterations=i+200;
+			
 	    }
             printf ("Time: %5.5lf, Cost: %5.5lf\n", 
                     ((double)(ts_now() - time_start))/1000000.0, (opttree->lower_bound)*k);   
@@ -404,49 +339,16 @@ else {
     printf("\n\n");
 
     //4.Print results
-	fprintf(planisphere,"{\n\"type\": \"FeatureCollection\",\n\n\"features\": [\n");
-    //4.1Print obstacles
-	/*fprintf(planisphere,"{\n\"type\": \"FeatureCollection\",\n\n\"features\": [\n");
-	while(obstacle_list){	
-		fprintf(planisphere,"{ \"type\": \"Feature\", \"properties\": { \"id\": 0 }, \"geometry\": { \"type\": \"Polygon\", \"coordinates\": [ [");
-        
-		GSList *obstacle = NULL;
-		obstacle = g_slist_append (obstacle, (obstacle_list->data));
-		while(obstacle){
-		state_t *state = (state_t *)(obstacle->data);
-                fprintf(planisphere,"[%3.5lf,%3.5lf]",state->x[0],state->x[1]);
-		obstacle=g_slist_next(obstacle);
-		}
-		fprintf(planisphere,"] ] } }\n\n,\n");
-		obstacle_list = g_slist_next (obstacle_list);			
-	}*/
-		
-
-    // 4.2Print the path
-   
+	fprintf(planisphere,"{\n\"type\": \"FeatureCollection\",\n\n\"features\": [\n");		
 	fprintf(planisphere,"{\"type\": \"Feature\", \"id\": 0, \"properties\": {}, \"geometry\": { \"type\": \"LineString\", \"coordinates\": [");
-
-
-
 	FILE* f_ptr = fopen ("optpath.txt", "w"); 
 
         GSList *optstates_list = NULL;
-	double tx[300];
-	double ty[300];
-	double tx_i[300];
-	double ty_i[300];
-	double tx2[300];
-	double ty2[300];
-	double tx2_i[300];
-	double ty2_i[300];
-	double tx3[300];
-	double ty3[300];
-	double tx3_i[300];
-	double ty3_i[300];
-	double tx_f[3000];
-	double ty_f[3000];
-	double tx_x[3000];
-	double ty_y[3000];
+	double *tx;
+	double *ty;
+	double *tx_i;
+	double *ty_i;
+
 	// list des points formants le chemmin ideal
 	
 	
@@ -457,12 +359,7 @@ else {
             node_curr = node_curr->parent;
         }
     	//modiiif
-	/*tx = (double *) malloc (g_slist_length(optstates_list)*sizeof(double));
-	ty = (double *) malloc (g_slist_length(optstates_list)*sizeof(double));
-	tx_i = (double *) malloc (g_slist_length(optstates_list)*sizeof(double));
-	tx_i = (double *) malloc (g_slist_length(optstates_list)*sizeof(double));
-	*/
-	
+
 
 
     	if (optstates_list) {
@@ -485,7 +382,7 @@ else {
 			} 				
                   fprintf (f_ptr, "%3.5lf, %3.5lf\n", 
                      state_this->x[0], state_this->x[1]);
-				  
+		 
 		  traj_from_parent_ptr = g_slist_next (traj_from_parent_ptr);
             }
 
@@ -503,21 +400,18 @@ else {
 			}     
              fprintf (f_ptr, "%3.5lf %3.5lf\n", 
                      state_curr->x[0], state_curr->x[1]);
+	     
             optstates_ptr = g_slist_next (optstates_ptr);
         }
     	}
 	
     	fclose (f_ptr);
-    	/*f_ptr = fopen ("optpath.txt", "r");
-	fprintf (planisphere,"[%3.5lf, %3.5lf,0]",root_state.x[0]*20,root_state.x[1]*20);
-	while( fscanf(f_ptr,"%lf %lf",&x,&y)!=EOF){
-   		fprintf (planisphere,",[%3.5lf, %3.5lf,0]",x*20,y*20);
-    	        fscanf(f_ptr,"\n");
-	} 
-	fclose(f_ptr);*/
+    	
     	//algo modif
 
-		//parcours de haut en bas
+
+		tx = (double *) malloc (g_slist_length(optstates_list)*2*sizeof(double));
+		ty = (double *) malloc (g_slist_length(optstates_list)*2*sizeof(double));
 		f_ptr = fopen ("optpath.txt", "r");
 		tx[0]=root_state.x[0];
 		ty[0]=root_state.x[1];
@@ -532,8 +426,10 @@ else {
     	        	fscanf(f_ptr,"\n");
 			
 		} 
-		
+//opt0	
 		int nb=i;
+		tx_i = (double *) malloc (g_slist_length(optstates_list)*2*sizeof(double));
+		ty_i = (double *) malloc (g_slist_length(optstates_list)*2*sizeof(double));
 		tx_i[0]=tx[nb];
 		ty_i[0]=ty[nb];
 		//indice tableau resultat
@@ -565,266 +461,8 @@ else {
 		int nb_point=k_;
 		k_--; 
 		printf("nb_point=%d\n",nb_point);
-		/*
-		//parcours de bas en haut
-
-		tx2[0]=tx_i[0];
-		ty2[0]=ty_i[0];
-				
-		int ii=0;
-		for(ii=1;ii<k+1;ii++){  
-			tx2[2*ii-1]=(tx_i[ii]+tx2[2*ii-2])/2;
-			ty2[2*ii-1]=(ty_i[ii]+ty2[2*ii-2])/2; 		
-			tx2[2*ii]=tx_i[ii];
-			ty2[2*ii]=ty_i[ii];   		
-		} 
-		int nb2=2*k;
-		tx2_i[0]=tx2[nb2];
-		ty2_i[0]=ty2[nb2];
-		//indice tableau resultat
-		k = 1;
-		i=nb2;
-		printf("zaaaaaab\n");
-		printf("%d\n",i);
 		
-		while (i != 0) {
-			state_t state2_a = {
-        				.x = {tx2[i], ty2[i]}
-    				};			
-			b=0;
-			j=0;
-			while( !b ){
-				state_t state2_b = {
-        				.x = {tx2[j], ty2[j]}
-    				};
-				if(optsystem_segment_on_obstacle (opttree->optsys, &state2_a, &state2_b, 100)==0){
-					tx2_i[k]=tx2[j];
-					ty2_i[k]=ty2[j];
-					k++;
-					i=j;
-					b=1;
-				}
-			
-			j++;
-			
-			}
-			printf("%d\n",i);
-		}
-		k--;*/
-/*
-		//parcours tous points->milieux
-
-		tx3[0]=tx2_i[0];
-		ty3[0]=ty2_i[0];
-				
-		ii=0;
-		for(ii=1;ii<k+1;ii++){  
-			tx3[2*ii-1]=(tx2_i[ii]+tx3[2*ii-2])/2;
-			ty3[2*ii-1]=(ty2_i[ii]+ty3[2*ii-2])/2; 		
-			tx3[2*ii]=tx2_i[ii];
-			ty3[2*ii]=ty2_i[ii];   		
-		} 
-		int nb3=2*k;
-		tx3_i[0]=tx3[nb3];
-		ty3_i[0]=ty3[nb3];
-		printf("%lf , %lf \n",tx3_i[0],ty3_i[0]);
-		//indice tableau resultat
-		k = 1;
-		i=nb3;
-		printf("zaaaaaab\n");
-		printf("%d\n",i);
-		
-		while (i != 0) {
-			
-			state_t state3_a = {
-        				.x = {tx3[i], ty3[i]}
-    				};			
-			b=0;
-			j=0;
-			while( !b ){
-				
-				if ((j%2)==1){
-					
-					state_t state3_b = {
-        					.x = {tx3[j], ty3[j]}
-    					};
-					if(optsystem_segment_on_obstacle (opttree->optsys, &state3_a, &state3_b, 100)==0){
-						
-						tx3_i[k]=tx3[j];
-						ty3_i[k]=ty3[j];
-						k++;
-						i=j;
-						b=1;
-					}
-					
-					
-				}
-				if (i==j) {
-						tx3_i[k]=tx3[j-1];
-						ty3_i[k]=ty3[j-1];
-						k++;
-						i=j-1;
-						b=1;
-							
-					}
-				j++;
-			}
-			printf("%d\n",i);
-		}
-		k--;
-*/
-//optimisation chemin
-//tx_f[0]=tx_i[0];
-//ty_f[0]=ty_i[0];
-/*
-tx[0]=root_state.x[0];
-ty[0]=root_state.x[1];
-i=0;
-while( fscanf(f_ptr,"%lf %lf",&x,&y)!=EOF){
-			i++;  	
-			tx[i]=x;
-			ty[i]=y;   		
-			fscanf(f_ptr,"\n");
-} 
-int nb=i;
-int nb_point=0;
-tx_f[0]=tx[0];
-ty_f[0]=ty[0];
-for (i=0;i<nb;i++){
-	x=-(tx[i]-tx[i+1])/10;
-	y=-(ty[i]-ty[i+1])/10;
-	for ( k=1;k<11;k++) {
-		tx_f[k+10*i]=tx_f[k+10*i-1]+x;
-		ty_f[k+10*i]=ty_f[k+10*i-1]+y;
-		nb_point++;
-	}
-}
-printf("nb_point=%d\n",nb_point);
-k=nb_point;
-int ind=0;
-int nb_final=0;
-i=0;
-//for(i=0;i<nb_point;i++){
-while(ind!=nb_point){
-	state_t state_i = {
-        				.x = {tx_f[i], ty_f[i]}
-    				};	
-				
-	state_t state_f = {
-        				.x = {tx_f[nb_point], ty_f[nb_point]}
-    				};			
-	
-	gboolean b=0;
-	state_t state_trans = state_f ;
-	int cpt=nb_point;
-	while(!b) {
-		if ((cpt-ind<11) && (cpt%10==0)){
-			tx_f[i+1]=tx_f[ind+1];
-			ty_f[i+1]=ty_f[ind+1];
-			b=1;
-			ind++;
-			nb_final++;
-		} else if(optsystem_segment_on_obstacle (opttree->optsys, &state_i, &state_trans, 100)==0){
-			printf("tasli7\n");			
-			tx_f[i+1]= state_trans.x[0];
-			ty_f[i+1]= state_trans.x[1];
-			b=1;
-			ind=cpt;
-			nb_final++;
-		}
-		cpt--;
-		state_trans.x[0]=tx_f[cpt];
-		state_trans.x[1]=ty_f[cpt];
-		
-	}
-i++;
-}
-
-
-		tx_i[0]=tx_f[nb_final];
-		ty_i[0]=ty_f[nb_final];
-		//indice tableau resultat
-		k = 1;
-		i=nb_final;
-		while (i != 0) {
-			state_t state_a = {
-        				.x = {tx_f[i], ty_f[i]}
-    				};			
-			b=0;
-			j=0;
-			while( !b ){
-				state_t state_b = {
-        				.x = {tx_f[j], ty_f[j]}
-    				};
-				if(optsystem_segment_on_obstacle (opttree->optsys, &state_a, &state_b, 100)==0){
-					tx_i[k]=tx_f[j];
-					ty_i[k]=ty_f[j];
-					k++;
-					i=j;
-					b=1;
-				}
-			
-			j++;
-			}
-			printf("%d\n",i);
-		}
-		k--;
-		int nb_points=k;
-		
-
-
-for (i=0;i<nb_points;i++){
-	x=-(tx_i[i]-tx_i[i+1])/10;
-	y=-(ty_i[i]-ty_i[i+1])/10;
-	for ( k=1;k<11;k++) {
-		tx_f[k+10*i]=tx_f[k+10*i-1]+x;
-		ty_f[k+10*i]=ty_f[k+10*i-1]+y;
-		nb_point++;
-	}
-}
-printf("nb_point=%d\n",nb_point);
-k=nb_point;
-ind=0;
-nb_final=0;
-i=0;
-//for(i=0;i<nb_point;i++){
-while(ind!=nb_point){
-	state_t state_i = {
-        				.x = {tx_f[i], ty_f[i]}
-    				};	
-				
-	state_t state_f = {
-        				.x = {tx_f[nb_point], ty_f[nb_point]}
-    				};			
-	
-	gboolean b=0;
-	state_t state_trans = state_f ;
-	int cpt=nb_point;
-	while(!b) {
-		if ((cpt-ind<11) && (cpt%10==0)){
-			tx_f[i+1]=tx_f[ind+1];
-			ty_f[i+1]=ty_f[ind+1];
-			b=1;
-			ind++;
-			nb_final++;
-		} else if(optsystem_segment_on_obstacle (opttree->optsys, &state_i, &state_trans, 100)==0){
-			printf("tasli7\n");			
-			tx_f[i+1]= state_trans.x[0];
-			ty_f[i+1]= state_trans.x[1];
-			b=1;
-			ind=cpt;
-			nb_final++;
-		}
-		cpt--;
-		state_trans.x[0]=tx_f[cpt];
-		state_trans.x[1]=ty_f[cpt];
-		
-	}
-i++;
-}
-*/
-
-
+//opt1
 for(i=0;i<nb_point-2;i++){
 	state_t state_0 = {
         				.x = {tx_i[i], ty_i[i]}
@@ -852,8 +490,8 @@ for(i=0;i<nb_point-2;i++){
 		state_trans.x[1]=state_trans.x[1]+y;
 	}
 }
-
-for(i=nb_point-1;i>1;i--){
+	//opt2
+	for(i=nb_point-1;i>1;i--){
 	state_t state_0 = {
         				.x = {tx_i[i], ty_i[i]}
     				};	
@@ -880,10 +518,13 @@ for(i=nb_point-1;i>1;i--){
 	}
 }
 
-//derniere optim
 
-	double px[300];
-	double py[300];
+	//opt3
+	
+	double *px;
+	double *py;
+	px = (double *) malloc (g_slist_length(optstates_list)*4*sizeof(double));
+	py = (double *) malloc (g_slist_length(optstates_list)*4*sizeof(double));
 	double d1x,d1y,d2x,d2y;
 	gboolean b1,b2;
 	px[0]=tx_i[0];
@@ -916,7 +557,7 @@ for(i=nb_point-1;i>1;i--){
 			
 			if (optsystem_segment_on_obstacle (opttree->optsys, &state_0, &state_1, 100) == 0){
 				k1++;
-				//printf("(%lf,%lf),(%lf,%lf)\n",state_0.x[0]*20,state_0.x[1]*20,state_1.x[0]*20,state_1.x[1]*20);
+				
 				
 			}
 			else {
@@ -931,18 +572,17 @@ for(i=nb_point-1;i>1;i--){
 				.x = {tx_i[i]+k1*d2x, ty_i[i]+k1*d2y}
 			};
 			while((b2) && (k2<5)){
-				//printf("%d\n",k2);
+				
 				state_t state_2 = {
         				.x = {tx_i[i]+k2*d1x, ty_i[i]+k2*d1y}
 				};
 			
 				if(optsystem_segment_on_obstacle (opttree->optsys, &state_1, &state_2, 100)==0){
 					k2++;
-					//if (k2<20)
-					//printf("(%lf,%lf),(%lf,%lf)\n",state_2.x[0]*20,state_2.x[1]*20,state_1.x[0]*20,state_1.x[1]*20);
+					
 				}
 				else {
-					//printf("%d\n",k2);
+					
 					b2=0;
 					k2--;
 				}
@@ -986,16 +626,15 @@ for(i=nb_point-1;i>1;i--){
 	py[j]=ty_i[nb_point-1];
 	nb_final=j;
 	
-
-	double px1[300];
-	double py1[300];
-	//double d1x,d1y,d2x,d2y;
-	//gboolean b1,b2;
+	//opt4
+	double *px1;
+	double *py1;
+	px1 = (double *) malloc (g_slist_length(optstates_list)*8*sizeof(double));
+	py1 = (double *) malloc (g_slist_length(optstates_list)*8*sizeof(double));
 	px1[0]=px[nb_final];
 	py1[0]=py[nb_final];
 	j=1;
-	//int nb_final;
-	//int k1, k2;
+	
 	for(i=nb_final-1;i>0;i--){
 		
 		d1x=(px[i-1]-px[i])/5;
@@ -1020,7 +659,7 @@ for(i=nb_point-1;i>1;i--){
 			
 			if (optsystem_segment_on_obstacle (opttree->optsys, &state_0, &state_1, 100) == 0){
 				k1++;
-				//printf("(%lf,%lf),(%lf,%lf)\n",state_0.x[0],state_0.x[1],state_1.x[0],state_1.x[1]);
+				
 				
 			}
 			else {
@@ -1084,75 +723,6 @@ for(i=nb_point-1;i>1;i--){
 	px1[j]=px[0];
 	py1[j]=py[0];
 	int nb_final1=j;
-		
-		
-			
-
-
-
-
-
-
-
-
-
-
-/*
-
-		tx2[0]=tx_i[k];
-		ty2[0]=ty_i[k];
-		int ii=0;
-		for(ii=1;ii<k;ii++){  
-			tx2[2*ii-1]=(tx_i[k-ii]+tx2[2*ii-2])/2;
-			ty2[2*ii-1]=(ty_i[k-ii]+ty2[2*ii-2])/2; 		
-			tx2[2*ii]=tx_i[k-ii];
-			ty2[2*ii]=ty_i[k-ii];   		
-			
-		} 
-		
-		int nb2=2*ii;
-		tx2_i[0]=tx2[0];
-		ty2_i[0]=ty2[0];
-		//indice tableau resultat
-		k = 1;
-		i=0;
-		while (i != nb2) {
-			state_t state2_a = {
-        				.x = {tx2[i], ty2[i]}
-    				};			
-			b=0;
-			j=nb2;
-			while( !b ){
-				state_t state2_b = {
-        				.x = {tx2[j], ty2[j]}
-    				};
-				if(optsystem_segment_on_obstacle (opttree->optsys, &state2_a, &state2_b, 100)==0){
-					tx2_i[k]=tx2[j];
-					ty2_i[k]=ty2[j];
-					k++;
-					i=j;
-					b=1;
-				}
-			
-			j--;
-			}
-			printf("%d\n",i);
-		}
-		printf("zaaaaaab\n");
-	k--;
-		printf("%d\n",k);
-*/	
-		//le ,	
-	/*printf("zaaaaaab\n");
-	printf("%d\n",nb_point);
-	k=nb_point-1;
-	fprintf (planisphere,"[%3.5lf, %3.5lf,0]",tx_i[k]*20,ty_i[k]*20);
-	while( k!=0 ){
-		printf("%d\n",k);
-		k--;
-   		fprintf (planisphere,",[%3.5lf, %3.5lf,0]",tx_i[k]*20,ty_i[k]*20);
-	} 
-*/
 
 	printf("nb_final1=%d\n",nb_final1);
 	k_=nb_final1;
@@ -1172,8 +742,9 @@ for(i=nb_point-1;i>1;i--){
 
 	fclose(planisphere);
 
-	//printf("distance: %lf\n",d);
-    // 5. Destroy the opttree structure
+	printf("distance: %lf\n",dist);
+	printf("k=%d\n",k);    
+	// 5. Destroy the opttree structure
     	opttree_destroy (opttree);
 	
     printf("%5.5lf\n",((double)(ts_now() - t))/1000000.0);
